bd17a8fc76ba89ec521bbc7f242d4873bd178247
maticamisay <maticamisay99@hotmail.com>
2022-01-09 16:36:58 -0300

    carga de productos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    productos: [
+      { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg' },
+      { name: 'Arbejas', price: 2500, img: '/productos/arbejas.jpg' },
+      { name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg' },
+    ],
+  }
+
   render(){
     return(
       <div>
