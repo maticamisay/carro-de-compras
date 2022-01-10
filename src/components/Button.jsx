1ade8a2731f25887e81488ec8f671a897fd3a80c
maticamisay <maticamisay99@hotmail.com>
2022-01-10 15:37:30 -0300

    centrando el contenido con un layout

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 const styles = {
-    button: {
-      backgroundColor: '#0A283E',
-      color: '#fff',
-      padding: '15px 20px',
-      border: 'none',
-      borderRadius: '5px',
-      cursor: 'pointer',
-    }
-  }
+  button: {
+    backgroundColor: "#0A283E",
+    color: "#fff",
+    padding: "15px 20px",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+  },
+};
 
 export default class Button extends Component {
-    render() {
-        return (
-            <button style={styles.button} {...this.props} />
-        )
-    }
+  render() {
+    return <button style={styles.button} {...this.props} />;
+  }
 }
