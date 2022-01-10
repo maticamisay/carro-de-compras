d0b69b9d64ef705dfcdb4eae84abdb6261c00707
maticamisay <maticamisay99@hotmail.com>
2022-01-10 15:47:40 -0300

    Agregando Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+const styles = {
+  navbar: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    height: "100px",
+    justifyContent: "space-between",
+    position: "relative",
+    padding: "0 50px",
+    boxShadow: "0 2px 3px rgb(0,0,0,0.1)",
+  },
+};
+export default class Navbar extends Component {
+  render() {
+
+    return (
+      <nav style={styles.navbar}>
+        <p>Logo</p>
+        <p>Carro</p>
+      </nav>
+    );
+  }
+}
