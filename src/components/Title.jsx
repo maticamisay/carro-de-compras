642c8b5150b4997e9117df4624185d11b02611e2
maticamisay <maticamisay99@hotmail.com>
2022-01-10 15:40:02 -0300

    añadimos un título

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const styles = {
+  title: {
+    marginBottom: "30px",
+  },
+};
+export default class Title extends Component {
+  render() {
+    return <h1 style={styles.title}>Tienda</h1>;
+  }
+}
