e9900d3fbcb57bf0e9300387fba9f22d41169ce0
maticamisay <maticamisay99@hotmail.com>
2022-01-11 14:09:19 -0300

    creamos el carro en un estado

--- src/App.js
+++ src/App.js
@@ -11,16 +11,39 @@ class App extends Component {
       { name: "Arbejas", price: 2500, img: "/productos/arbejas.jpg" },
       { name: "Lechuga", price: 500, img: "/productos/lechuga.jpg" },
     ],
+    carro: [],
+  };
+
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state;
+    if (carro.find((x) => x.name === producto.name)) {
+      const newCarro = carro.map((x) =>
+        x.name === producto.name
+          ? {
+              ...x,
+              cantidad: x.cantidad + 1,
+            }
+          : x
+      );
+      return this.setState({ carro: newCarro });
+    }
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      }),
+    });
   };
 
   render() {
+    console.log(this.state.carro);
     return (
       <div>
         <Navbar />
         <Layout>
           <Title />
           <Productos
-            agregarAlCarro={() => console.log("No hace nada")}
+            agregarAlCarro={this.agregarAlCarro}
             productos={this.state.productos}
           />
         </Layout>
